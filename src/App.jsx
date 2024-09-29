22c2382e13a35663b64474bfb602abfe30b6caa2
Wesley Gado <wesleygado@gmail.com>
2024-09-29 15:49:47 -0300

    Roteamento com objetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes, useRoutes } from 'react-router-dom'
 import './App.css'
 import Index from './pages'
 import About from './pages/about'
 import Produtos, { Produto } from './pages/produtos'
 
 function App() {
+  const element = useRoutes([
+    {
+      path: '',
+      element: <Index />
+    },
+    {
+      path: 'produtos/*',
+      element: <Produtos />,
+      children: [
+        {
+          path: ':id',
+          element: <Produto />
+        }
+      ]
+    }
+  ]);
+
   return (
     <div>
-      <Routes>
+      {element}
+     {/*  <Routes>
         <Route path='/' element={<Index />} />
         <Route path='/about/:id' element={<About />} />
         <Route path='/produtos/*' element={<Produtos />} >
           <Route path=":id" element={<Produto />} />
         </Route>
-      </Routes>
+      </Routes> */}
 
-    {/*  <Link to='/'>Home</Link>
+      {/*  <Link to='/'>Home</Link>
       <br />
       <Link to='/produtos'>Produtos</Link>
       <br />
